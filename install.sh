f8508b08c51a8103a7035fd71924511a4dc4ea36
Joeri Kassenaar <joeri.kassenaar@centagon.com>
2026-01-12 13:10:53 +0100

    fix for install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,21 @@ else
     echo "Creating $ENV_FILE from example..."
     cp ./backend/.env.example "$ENV_FILE"
 
-    echo "$ENV_FILE created."
+    # Configure for Docker environment
+    sed -i '' 's/DB_CONNECTION=sqlite/DB_CONNECTION=mysql/g' "$ENV_FILE"
+    sed -i '' 's/# DB_HOST=127.0.0.1/DB_HOST=mysql/g' "$ENV_FILE"
+    sed -i '' 's/# DB_PORT=3306/DB_PORT=3306/g' "$ENV_FILE"
+    sed -i '' 's/# DB_DATABASE=nexus_noir/DB_DATABASE=nexus_noir/g' "$ENV_FILE"
+    sed -i '' 's/# DB_USERNAME=root/DB_USERNAME=user/g' "$ENV_FILE"
+    sed -i '' 's/# DB_PASSWORD=/DB_PASSWORD=password/g' "$ENV_FILE"
+    
+    sed -i '' 's/REDIS_HOST=127.0.0.1/REDIS_HOST=redis/g' "$ENV_FILE"
+    
+    sed -i '' 's/MAIL_MAILER=log/MAIL_MAILER=smtp/g' "$ENV_FILE"
+    sed -i '' 's/MAIL_HOST=127.0.0.1/MAIL_HOST=mailpit/g' "$ENV_FILE"
+    sed -i '' 's/MAIL_PORT=2525/MAIL_PORT=1025/g' "$ENV_FILE"
+
+    echo "$ENV_FILE created and configured for Docker."
 fi
 
 # Create storage symbolic link for Laravel (using relative path for Docker compatibility)
